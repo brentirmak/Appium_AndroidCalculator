8ea31a4a7ba1096c1941250dbf2984dff2b09ad2
Brent Irmak <brent.irmak@gmail.com>
2026-05-14 10:43:51 -0700

    Add logic to store transactional data into MySQL

--- Calculator.py
+++ Calculator.py
@@ -3,6 +3,55 @@ from appium.webdriver.common.appiumby import AppiumBy
 import time
 from appium.options.common import AppiumOptions
 from contextlib import contextmanager
+import mysql.connector
+from datetime import datetime
+import pytz
+
+trx_dict = {}
+
+def store_to_mysql(trx_dict):
+    print(trx_dict)
+    #print(type(trx_dict['Home']))
+    #print(type(trx_dict['Access Basic Calculator']))
+    #print(type(trx_dict['Perform Basic Calculation']))
+    #print(type(trx_dict['Go Back To Home']))
+
+    if "Home" in trx_dict:
+        home_transaction = round(trx_dict['Home'],4)
+    else:
+        home_transaction = 'NULL'
+    if "Access Basic Calculator" in trx_dict:
+        access_basic_calculator_transaction = round(trx_dict['Access Basic Calculator'],4)
+    else:
+        access_basic_calculator_transaction = 'NULL'
+    if "Perform Basic Calculation" in trx_dict:
+        perform_basic_calculation_transaction = round(trx_dict['Perform Basic Calculation'],4)
+    else:
+        perform_basic_calculation_transaction = 'NULL'
+    if "Go Back To Home" in trx_dict:
+        go_back_to_home_transaction = round(trx_dict['Go Back To Home'],4)
+    else:
+        go_back_to_home_transaction = 'NULL'
+
+
+    current_timestamp = datetime.now(pytz.timezone('America/Los_Angeles'))
+
+    conn = mysql.connector.connect(
+        user = "selenium",
+        password = "Selenium#123#",
+        host = "192.168.239.1",
+        database="appium"
+    )
+    cursor = conn.cursor()
+    cursor.execute(
+        """INSERT INTO appium_calculator
+               (RunTimeStamp, Home, AccessBasicCalculator, PerformBasicCalculation, GoBackToHome)
+           VALUES (%s, %s, %s, %s, %s)""",
+        (current_timestamp,home_transaction,access_basic_calculator_transaction,perform_basic_calculation_transaction,go_back_to_home_transaction)
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 @contextmanager
@@ -16,6 +65,7 @@ def appium_transaction(name):
         end_time = time.time()
         duration = end_time - start_time
         print(f"Transaction {name} took {duration:.2f} seconds")
+        trx_dict.update({name: duration})
 
 options = AppiumOptions()
 options.load_capabilities({
@@ -49,7 +99,7 @@ try:
     print("Successfully connected to Windows Emulator!")
     print("Proceeding...")
 
-    with appium_transaction("Home"):
+    with appium_transaction("Home") as home_transaction:
         try:
             print("Checking the Home header")
             driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Home"]')
@@ -147,10 +197,16 @@ try:
         print("Script terminated successfully")
         print("************************************")
 
+
 finally:
+    store_to_mysql(trx_dict)
+
     print("Terminating the application")
     driver.terminate_app("calculator.currencyconverter.tipcalculator.unitconverter")
 
     time.sleep(2)
     print("Quitting the driver")
     driver.quit()
+
+
+
