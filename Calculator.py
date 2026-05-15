70952d491018329f927f78dfb81791f65a7ce36e
Brent Irmak <brent.irmak@gmail.com>
2026-05-14 19:24:44 -0700

    Adjust the try-except logic for the Home trx; add error snapshot capturing logic

--- Calculator.py
+++ Calculator.py
@@ -4,6 +4,8 @@ import time
 from appium.options.common import AppiumOptions
 from contextlib import contextmanager
 import StoreToMySQL
+import os
+from datetime import datetime
 
 trx_dict = {}
 device_info = []
@@ -20,6 +22,20 @@ def appium_transaction(name):
         duration = end_time - start_time
         print(f"Transaction {name} took {duration:.2f} seconds")
         trx_dict.update({name: duration})
+
+def capture_error_snapshot(driver, test_name):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    folder = f"Error_Snapshots/{test_name}"
+    os.makedirs(folder, exist_ok=True)
+
+    screenshot_path = os.path.join(
+        folder,
+        f"error_{timestamp}.png"
+    )
+    driver.save_screenshot(screenshot_path)
+    print(f"Saved screenshot: {screenshot_path}")
+
 
 options = AppiumOptions()
 options.load_capabilities({
@@ -80,34 +96,30 @@ try:
             driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Basic Calculator"] ')
             print("Basic Calculator icon found")
         except:
-            print("Home didn't load - will check to see if Ad screen is active")
-
-        try:
-            print("Checking to see if a Test Ad was loaded")
-            driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Test Ad"] | //android.webkit.WebView')
-            print("Test Ad displayed - need to close it")
             try:
+                print("Home didn't load - will check to see if Ad screen is active")
+                print("Checking to see if a Test Ad was loaded")
+                driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Test Ad"] | //android.webkit.WebView')
+                print("Test Ad displayed - need to close it")
                 driver.find_element(by=AppiumBy.XPATH, value='//android.view.View[@resource-id="dismiss-button"]').click()
                 print("Clicked on dismiss button to close Test Ad")
             except:
-                print("There was no close button for the Test Add pop-up - proceeding...")
-        except:
-            try:
-                print("Checking to see if there's a Close Ad link on the upper right corner of the Ad")
-                driver.find_element(by=AppiumBy.XPATH, value='//android.view.View[@resource-id="close-button"]/android.view.View/android.view.View/android.widget.Image').click()
-                print("Found Close Ad link on the upper right corner of the Ad - clicked on it")
-            except:
                 try:
-                    print("Checking to see if there's a Continue to app link on the upper right corner of the Ad")
-                    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Continue to app"]').click()
-                    print("Found Continue to app link on the upper right corner of the Ad - clicked on it")
+                    print("There was no close button for the Test Add pop-up - proceeding...")
+                    print("Checking to see if there's a Close Ad link on the upper right corner of the Ad")
+                    driver.find_element(by=AppiumBy.XPATH, value='//android.view.View[@resource-id="close-button"]/android.view.View/android.view.View/android.widget.Image').click()
+                    print("Found Close Ad link on the upper right corner of the Ad - clicked on it")
                 except:
                     try:
+                        print("Checking to see if there's a Continue to app link on the upper right corner of the Ad")
+                        driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Continue to app"]').click()
+                        print("Found Continue to app link on the upper right corner of the Ad - clicked on it")
                         print("Checking to see if there's a 'x' icon on the upper right corner")
                         driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Image').click()
                         print("Clicked on the 'x' icon on the upper right corner")
                     except:
-                        print("No ad pop-up was displayed - proceeding...")
+                        capture_error_snapshot(driver, "Home")
+                        raise
 
     with appium_transaction("Access Basic Calculator"):
         try:
@@ -117,52 +129,63 @@ try:
         except:
             print("We are not at the Home screen")
 
-        print("Checking for the Basic Calculator header")
-        driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Basic Calculator"]')
-        print("Basic Calculator header was found")
-
+        try:
+            print("Checking for the Basic Calculator header")
+            driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Basic Calculator"]')
+            print("Basic Calculator header was found")
+        except Exception as e:
+            capture_error_snapshot(driver, "AccessBasicCalculator")
+            raise
     with appium_transaction("Perform Basic Calculation"):
-        print("Looking for the delete icon")
-        driver.find_element(by=AppiumBy.XPATH, value='//android.widget.ImageView[@resource-id="calculator.currencyconverter.tipcalculator.unitconverter:id/btn_delete"]')
-        print("Found the delete icon")
+        try:
+            print("Looking for the delete icon")
+            driver.find_element(by=AppiumBy.XPATH, value='//android.widget.ImageView[@resource-id="calculator.currencyconverter.tipcalculator.unitconverter:id/btn_delete"]')
+            print("Found the delete icon")
 
-        print("Looking for the 'C' button")
-        driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@resource-id="calculator.currencyconverter.tipcalculator.unitconverter:id/btn_clear"]').click()
-        print("Found the 'C' button - clicked on it")
+            print("Looking for the 'C' button")
+            driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@resource-id="calculator.currencyconverter.tipcalculator.unitconverter:id/btn_clear"]').click()
+            print("Found the 'C' button - clicked on it")
 
-        print("Clicking on 9")
-        driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_num9').click()
-        print("Clicked on 9")
+            print("Clicking on 9")
+            driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_num9').click()
+            print("Clicked on 9")
 
-        print("Clicking on '+'")
-        driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_plus').click()
-        print("Clicked on '+'")
+            print("Clicking on '+'")
+            driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_plus').click()
+            print("Clicked on '+'")
 
-        print("Clicking on 9 again")
-        driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_num9').click()
-        print("Clicked on 9 again")
+            print("Clicking on 9 again")
+            driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_num9').click()
+            print("Clicked on 9 again")
 
-        print("Looking for the '=' icon")
-        driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@resource-id="calculator.currencyconverter.tipcalculator.unitconverter:id/btn_result"]').click()
-        print("Found the '=' icon")
+            print("Looking for the '=' icon")
+            driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@resource-id="calculator.currencyconverter.tipcalculator.unitconverter:id/btn_result"]').click()
+            print("Found the '=' icon")
 
-        print("Looking for result")
-        output = driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/tvResult').text
-        print(f"Test Result: 9 + 9 {output}")
-        print("Found the result")
+            print("Looking for result")
+            output = driver.find_element(by=AppiumBy.ID, value='calculator.currencyconverter.tipcalculator.unitconverter:id/tvResult').text
+            print(f"Test Result: 9 + 9 {output}")
+            print("Found the result")
+        except Exception as e:
+            capture_error_snapshot(driver, "PerformBasicCalculation")
+            raise
 
     with appium_transaction("Go Back To Home"):
-        print("Clicking on the menu")
-        driver.find_element(by=AppiumBy.ID,value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_open_side_menu').click()
-        print("Clicked on the menu")
+        try:
+            print("Clicking on the menu")
+            driver.find_element(by=AppiumBy.ID,value='calculator.currencyconverter.tipcalculator.unitconverter:id/btn_open_side_menu').click()
+            print("Clicked on the menu")
 
-        print("Clicking on the Home link")
-        driver.find_element(by=AppiumBy.XPATH,value='//android.widget.TextView[@text="Home"]').click()
-        print("Clicked on the Home link")
+            print("Clicking on the Home link")
+            driver.find_element(by=AppiumBy.XPATH,value='//android.widget.TextView[@text="Home"]').click()
+            print("Clicked on the Home link")
 
-        print("Checking the Tip Calculator option")
-        driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Tip Calculator"]')
-        print("Found the Tip Calculator option")
+            print("Checking the Tip Calculator option")
+            driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Tip Calculator"]')
+            print("Found the Tip Calculator option")
+        except Exception as e:
+            capture_error_snapshot(driver, "GoBackToHome")
+            raise
 
         print("************************************")
         print("Script terminated successfully")
