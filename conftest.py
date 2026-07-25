276d1e75c981a57bd25ea948ae3104d76ca517cd
Brent Irmak <brent.irmak@gmail.com>
2026-07-24 22:21:38 -0700

    Made 2 tests POM friendly; 2nd test is dependent on the 1st test

--- conftest.py
+++ conftest.py
@@ -3,18 +3,29 @@ import time
 from utils.driver_factory import create_android_driver
 
 TIMEOUT = 30
-
-@pytest.fixture
+@pytest.fixture(scope="session")
 def driver():
     driver = create_android_driver()
-    time.sleep(15)
     yield driver
-
+    # 1. Close the app if it's running
     try:
         driver.terminate_app("calculator.currencyconverter.tipcalculator.unitconverter")
     except Exception:
         pass
 
+    # 2. Force-stop the app (more reliable on Android 17)
+    try:
+        driver.execute_script(
+            "mobile: shell",
+            {
+                "command": "am",
+                "args": ["force-stop", "calculator.currencyconverter.tipcalculator.unitconverter"]
+            }
+        )
+    except Exception:
+        pass
+
+    # 3. Quit the Appium session
     try:
         driver.quit()
     except Exception:
