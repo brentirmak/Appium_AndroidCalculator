614af0fa9ce8557ca8d2dbaad75c30625bfa51d7
Brent Irmak <brent.irmak@gmail.com>
2026-09-09 10:16:32 -0700

    Add new column to designate runtype - manual vs jenkins

--- utils/StoreToMySQL.py
+++ utils/StoreToMySQL.py
@@ -2,21 +2,23 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
-# Get the absolute path of the directory containing this script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-env_path = os.path.join(script_dir, ".env")
+# ============================================================
+# Load environment variables
+# ============================================================
 
-# Force load_dotenv to look in the script's directory
-load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 mysql_host = os.getenv("MYSQL_URL")
 mysql_username = os.getenv("MYSQL_USERNAME")
 mysql_password = os.getenv("MYSQL_PASSWORD")
+run_type = ""
 
-# Debugging check (will fail safely if variables are missing)
-if not mysql_password:
-    print(f"DEBUG ERROR: MYSQL_PASSWORD was not found! Loaded from: {env_path}")
-
+if "JENKINS_SERVER_COOKIE" in os.environ or "BUILD_NUMBER" in os.environ:
+    print("\nWe are running from Jenkins")
+    run_type = "jenkins"
+else:
+    print("\nWe are NOT running from Jenkins - need to set MySQL URL accordingly")
+    run_type = "manual"
 
 def store_transaction_result(test_name, transaction, status, duration, timestamp):
     try:
@@ -30,10 +32,10 @@ def store_transaction_result(test_name, transaction, status, duration, timestamp
 
         cursor.execute(
             """
-            INSERT INTO appium_android_calculator (RunTimeStamp, TestName, Transaction, Status, Duration)
-            VALUES (%s, %s, %s, %s, %s)
+            INSERT INTO appium_android_calculator (RunTimeStamp, RunType, TestName, Transaction, Status, Duration)
+            VALUES (%s, %s, %s, %s, %s, %s)
         """,
-            (timestamp, test_name, transaction, status, duration),
+            (timestamp, run_type, test_name, transaction, status, duration),
         )
 
         conn.commit()
